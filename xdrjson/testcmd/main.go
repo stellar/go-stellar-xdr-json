543848df4418291d9d791f5ddad8d62bf879fb33
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2025-02-24 11:38:14 +1000

    fix

---
+++ xdrjson/testcmd/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"os"
+
+	"github.com/stellar/go-stellar-xdr-json/xdrjson"
+)
+
+type Asset struct{}
+
+// A command-line tool to test that the xdr2json package compiles and runs on platforms.
+func main() {
+	xdrBase64 := "AAAAAUFCQ0QAAAAAY3NrWbIzxOdwa+BhWFLSODTyh99xwpyb1gTzTQjDvGE="
+	wantJson := `{"credit_alphanum4":{"asset_code":"ABCD","issuer":"GBRXG22ZWIZ4JZ3QNPQGCWCS2I4DJ4UH35Y4FHE32YCPGTIIYO6GCL64"}}`
+
+	fmt.Fprintf(os.Stderr, "xdr : %s\n", xdrBase64)
+	fmt.Fprintf(os.Stderr, "want: %s\n", wantJson)
+
+	rawBytes, err := base64.StdEncoding.DecodeString(xdrBase64)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: decoding base64: %v\n", err)
+		os.Exit(1)
+	}
+
+	json, err := xdrjson.ConvertBytes(Asset{}, rawBytes)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: converting binary to json: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Fprintf(os.Stderr, "got : %s\n", string(json))
+
+	if string(json) != wantJson {
+		fmt.Fprintf(os.Stderr, "FAIL\n")
+		os.Exit(1)
+	}
+
+	fmt.Fprintf(os.Stderr, "PASS\n")
+}
