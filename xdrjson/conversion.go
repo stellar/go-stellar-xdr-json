dc0a788a23333c3c08081e2636d3ebe289c181e1
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2025-03-13 09:42:55 +1000

    add back original fns and mark as deprecated

--- xdrjson/conversion.go
+++ xdrjson/conversion.go
@@ -16,13 +16,62 @@ package xdrjson
 import "C"
 
 import (
+	"encoding"
 	"encoding/json"
+	"reflect"
 	"unsafe"
 
 	"github.com/pkg/errors"
 )
 
+// ConvertBytes takes an XDR object (`xdr`) and its serialized bytes (`field`)
+// and returns the raw JSON-formatted serialization of that object.
+// It can be unmarshalled to a proper JSON structure, but the raw bytes are
+// returned to avoid unnecessary round-trips. If there is an
+// error, it returns an empty string.
+//
+// The `xdr` object does not need to actually be initialized/valid:
+// we only use it to determine the name of the structure. We could just
+// accept a string, but that would make mistakes likelier than passing the
+// structure itself (by reference).
+//
+// Deprecated: Use `Decode` instead.
+func ConvertBytes(xdr interface{}, field []byte) (json.RawMessage, error) {
+	if len(field) == 0 {
+		return []byte(""), nil
+	}
+
+	xdrTypeName := reflect.TypeOf(xdr).Name()
+	return convertAnyBytes(xdrTypeName, field)
+}
+
+// ConvertInterface takes a valid XDR object (`xdr`) and returns
+// the raw JSON-formatted serialization of that object. If there is an
+// error, it returns an empty string.
+//
+// Unlike `ConvertBytes`, the value here needs to be valid and
+// serializable.
+//
+// Deprecated: Use `Decode` instead.
+func ConvertInterface(xdr encoding.BinaryMarshaler) (json.RawMessage, error) {
+	xdrTypeName := reflect.TypeOf(xdr).Name()
+	data, err := xdr.MarshalBinary()
+	if err != nil {
+		return []byte(""), errors.Wrapf(err, "failed to serialize XDR type '%s'", xdrTypeName)
+	}
+
+	return convertAnyBytes(xdrTypeName, data)
+}
+
+// Decode takes an XDR base64 encoded value, and decodes it as the XdrType,
+// returning it's XDR-JSON representation as JSON.
+//
+// Returns the JSON message if decoding successful, otherwise an error.
 func Decode(xdrTypeName XdrType, field []byte) (json.RawMessage, error) {
+	return convertAnyBytes(string(xdrTypeName), field)
+}
+
+func convertAnyBytes(xdrTypeName string, field []byte) (json.RawMessage, error) {
 	var jsonStr, errStr string
 	// scope just added to show matching alloc/frees
 	{
--- xdrjson/conversion_test.go
+++ xdrjson/conversion_test.go
@@ -40,8 +40,48 @@ func TestDecode(t *testing.T) {
 	}
 }
 
-func TestEmptyConversion(t *testing.T) {
-	js, err := Decode(SorobanTransactionData, []byte{})
+func TestDecode_empty(t *testing.T) {
+	_, err := Decode(SorobanTransactionData, []byte{})
+	require.Error(t, err)
+}
+
+// We just need similarly named structs as are defined in the
+
+func TestConvertBytes_success(t *testing.T) {
+	type Asset struct{}
+	/* The base64-encoded string representing the asset
+	Created with:
+	$ stellar xdr encode --type Asset << -
+	{"credit_alphanum4":{"asset_code":"ABCD","issuer":"GBRXG22ZWIZ4JZ3QNPQGCWCS2I4DJ4UH35Y4FHE32YCPGTIIYO6GCL64"}}
+	-
+	*/
+	encodedAsset := "AAAAAUFCQ0QAAAAAY3NrWbIzxOdwa+BhWFLSODTyh99xwpyb1gTzTQjDvGE="
+
+	rawBytes, err := base64.StdEncoding.DecodeString(encodedAsset)
+	require.NoError(t, err)
+
+	jsb, err := ConvertBytes(Asset{}, rawBytes)
+	require.NoError(t, err)
+
+	var dest map[string]interface{}
+	require.NoError(t, json.Unmarshal(jsb, &dest))
+
+	// Ensure the asset has the correct fields
+	require.Contains(t, dest, "credit_alphanum4")
+	require.Contains(t, dest["credit_alphanum4"], "asset_code")
+	require.Contains(t, dest["credit_alphanum4"], "issuer")
+	require.IsType(t, map[string]interface{}{}, dest["credit_alphanum4"])
+
+	// Check the issuer address and asset code
+	if converted, ok := dest["credit_alphanum4"].(map[string]interface{}); assert.True(t, ok) {
+		require.Equal(t, "GBRXG22ZWIZ4JZ3QNPQGCWCS2I4DJ4UH35Y4FHE32YCPGTIIYO6GCL64", converted["issuer"])
+		require.Equal(t, "ABCD", converted["asset_code"])
+	}
+}
+
+func TestConvertBytes_empty(t *testing.T) {
+	type SorobanTransactionData struct{}
+	js, err := ConvertBytes(SorobanTransactionData{}, []byte{})
 	require.NoError(t, err)
 	require.Equal(t, "", string(js))
 }
